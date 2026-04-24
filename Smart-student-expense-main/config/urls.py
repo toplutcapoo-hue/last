8aca18e6461d5e8e122a22d896421dcec3ff2eea
toplutcapoo-hue <toplutcapoo@gmail.com>
2026-04-24 14:16:16 +0300

    Add logout path to Django URL configuration

--- Smart-student-expense-main/config/urls.py
+++ Smart-student-expense-main/config/urls.py
@@ -1,7 +1,10 @@
+
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views # Add this line
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'), # Add this line
     path('', include('core.urls')),
 ]
